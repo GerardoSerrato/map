caa0d3205c2b8110479e9e2e7ef4f4b7c350cbc8
Gerardo Serrato <gerardoserratoj@gmail.com>
2022-06-20 19:01:12 -0500

    Pruebas de JSON

--- tests/test_script.js
+++ tests/test_script.js
@@ -4,3 +4,17 @@ var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '© OpenStreetMap'
 }).addTo(map);
+
+var lunesManuel = [];
+
+document.getElementById("getFile").addEventListener("change", function() {
+  var file_to_read = document.getElementById("getFile").files[0];
+  var fileread = new FileReader();
+  fileread.onload = function(e) {
+    var content = e.target.result;
+    // console.log(content);
+    var intern = JSON.parse(content); // Array of Objects.
+    console.log(intern); // You can index every object
+  };
+  fileread.readAsText(file_to_read);
+});
