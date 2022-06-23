e17f5ba5b056983c7de46ec593151cbaa13393cd
Gerardo Serrato <gerardoserratoj@gmail.com>
2022-06-22 19:40:44 -0500

    LIsta de conductores y días activos

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+var listActive = [];
+
 function cambiarColor(valueInput) {
   var getInput = document.getElementById(valueInput);
-  console.log("se oprimió botón");
-  console.log(getInput.value);
   if (getInput.checked== true) {
-    console.log("Esta encendido");
+    listActive.push(valueInput);
+    console.log(listActive);
   }else {
-    console.log("No está encendido");
+    var removeElement = listActive.indexOf(valueInput);
+    listActive.splice(removeElement,1)
+    console.log(listActive);
   }
+}
+
+function llamadoAsi() {
+  console.log(listActive);
 }
